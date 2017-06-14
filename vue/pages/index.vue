34be83b2c7e2111bda15fc1f78ad2198bec8243b
potapovDim <potapovd3012@gmail.com>
2017-06-14 13:37:55 +0300

    begin

---
+++ vue/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>HELLLO</div>
+    <login></login>
+  </div>
+</template>
+
+<script>
+import login from './login'
+export default {
+  components: {
+    login
+  }
+}
+</script>
---
+++ vue/pages/login.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <input type="text" placeholder="Username" />
+    <button>Log in</button>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
